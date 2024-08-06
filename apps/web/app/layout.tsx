735a1a5f5753947641c30ccda6b49b217026e09a
ohbyeongmin <fbottlem@gmail.com>
2024-08-06 18:04:11 +0900

    Initial web

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Tokamak Rollup Hub",
+  description: "Hello, World! This is Tokamak Rollup Hub",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}

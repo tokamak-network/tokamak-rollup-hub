f0a02dd79381b0e68ec3fb58d761d4668143cc5d
ohbyeongmin <fbottlem@gmail.com>
2024-08-08 16:18:49 +0900

    style: custom tailwindcss

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,18 +1,32 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        lg: '1200px',
+        md: '800px',
+        sm: '360px',
       },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+    colors: {
+      'tokamak-blue': {
+        DEFAULT: '#0070ED',
+        dark: '#0057E6',
+      },
+      'trh-red': '#FF0420',
+      'trh-green': '#06D863',
+      'text-negative': '#FD3D51',
+      'text-warning': '#FF7E12',
     },
   },
   plugins: [],

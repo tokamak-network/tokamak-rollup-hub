7c37b8a1c816040b787bf781ca51d13d08386e15
ohbyeongmin <fbottlem@gmail.com>
2024-09-05 16:53:00 +0900

    fix: landing visual aligned verically middle in desktop

--- apps/web/app/(home)/layout.tsx
+++ apps/web/app/(home)/layout.tsx
@@ -1,6 +1,6 @@
 export default function HomeLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-screen w-full pt-[142px] md:pl-[45px] md:pr-0 xl:h-[calc(100vh-78px)] xl:px-[30px] 2xl:px-[150px]">
+    <div className="h-screen w-full pt-[142px] md:pl-[45px] md:pr-0 xl:flex xl:h-[calc(100vh-78px)] xl:flex-col xl:justify-center xl:px-[30px] 2xl:px-[150px]">
       {children}
     </div>
   );

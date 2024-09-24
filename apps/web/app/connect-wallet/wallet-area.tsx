c617e1fda1d9c7c7f5a1a8f575e59d812fdabb0c
ohbyeongmin <fbottlem@gmail.com>
2024-09-24 17:16:38 +0900

    feat: add theme switch btn

--- apps/web/app/connect-wallet/wallet-area.tsx
+++ apps/web/app/connect-wallet/wallet-area.tsx
@@ -8,6 +8,7 @@ import { useState } from 'react';
 import { copyToClipboard } from '@/lib/utils';
 import clsx from 'clsx';
 import { useDisconnect } from 'wagmi';
+import { ThemeSwitchBtn } from '@/components/buttons/theme-switch-btn';
 
 type addressType = `0x${string}` | undefined;
 
@@ -35,7 +36,7 @@ export function WalletArea({ address }: WalletAreaProps) {
       </div>
       <ul
         tabIndex={0}
-        className="dropdown-content z-50 mt-1.5 rounded-lg bg-black px-2 py-2 ring-2 ring-[#1D2838] *:transition-colors"
+        className="dropdown-content z-50 w-auto -translate-x-[25px] translate-y-2 rounded-lg bg-black px-[10px] py-[10.5px] ring-2 ring-[#1D2838] *:transition-colors"
       >
         <li>
           <button
@@ -44,7 +45,7 @@ export function WalletArea({ address }: WalletAreaProps) {
               copyToClipboard(text, setCopied);
             }}
             disabled={copied}
-            className="flex w-full gap-2 rounded-lg px-2 py-2 *:transition-all hover:bg-base-200"
+            className="flex w-full gap-2 rounded-lg px-2 py-[7.5px] *:transition-all hover:bg-base-200"
           >
             {copied ? <Image src={CopiedIcon} alt="copied" /> : <Image src={CopyIcon} alt="copy" />}
             <span
@@ -67,13 +68,16 @@ export function WalletArea({ address }: WalletAreaProps) {
                   disconnect({ connector });
                 }}
                 key={connector.id}
-                className="flex w-full gap-2 rounded-lg px-2 py-2 hover:bg-base-200"
+                className="flex w-full gap-2 rounded-lg px-2 py-[7.5px] hover:bg-base-200"
               >
                 <Image src={DisconnectIcon} alt="disconnect" />
                 <span className="whitespace-nowrap text-[13px] tracking-[0.26px]">Disconnect</span>
               </button>
             ) : null;
           })}
+        </li>
+        <li>
+          <ThemeSwitchBtn />
         </li>
       </ul>
     </div>

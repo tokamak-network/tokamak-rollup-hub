90afcfc7bd886f9acac61e938c7a4e75ae3a1c07
ohbyeongmin <fbottlem@gmail.com>
2024-09-24 13:02:12 +0900

    refactor: modal scroll

--- apps/web/components/modal.tsx
+++ apps/web/components/modal.tsx
@@ -1,6 +1,6 @@
 export default function Modal({ children }: { children: React.ReactNode }) {
   return (
-    <div className="absolute left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-black bg-opacity-75">
+    <div className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center overflow-y-hidden bg-black bg-opacity-75">
       {children}
     </div>
   );

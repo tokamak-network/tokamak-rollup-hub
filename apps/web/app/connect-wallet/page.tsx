2495f9b4adb21eaee95a7659c7fe3cfafb77109f
ohbyeongmin <fbottlem@gmail.com>
2024-09-24 09:46:17 +0900

    fix: wallet page height

--- apps/web/app/connect-wallet/page.tsx
+++ apps/web/app/connect-wallet/page.tsx
@@ -2,7 +2,7 @@ import WalletList from './wallet-list';
 
 export default function ConnectWallet() {
   return (
-    <div className="flex w-full items-center justify-center">
+    <div className="flex min-h-[calc(100vh-78px)] w-full items-center justify-center">
       <WalletList />
     </div>
   );

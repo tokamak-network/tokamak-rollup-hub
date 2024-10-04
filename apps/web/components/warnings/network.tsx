671f05c68b7a9a0b9dcacdefeb063bdb81a65dcd
ohbyeongmin <fbottlem@gmail.com>
2024-10-04 12:27:26 +0900

    chore: mod warning text

--- apps/web/components/warnings/network.tsx
+++ apps/web/components/warnings/network.tsx
@@ -4,11 +4,12 @@ export function ChangeNetworkToThanos() {
   return (
     <div className="absolute -left-[104px] top-[55px] z-50 animate-bounce">
       <div className="w-[240px] rounded-[3px] bg-white p-2 text-[12px] ring-[1px] ring-[#E8EDF2] dark:bg-[#13131A] dark:ring-[#4B5563]">
-        <p className="text-center">
-          Please connect to <span className="text-tokamak-blue">Thanos</span>
-        </p>
+        <p className="text-center">Wallet should be either connected</p>
         <p>
-          <span className="text-tokamak-blue">mainnet & testnet</span> to use this service.
+          <span className="text-tokamak-blue">
+            Ethereum mainnet <span className="text-[#1C1C1C] dark:text-white">or</span> Thanos
+            testnet.
+          </span>
         </p>
       </div>
       <div className="absolute bottom-[calc(100%-6px)] left-[calc(50%-4.5px)] -z-10 w-2">

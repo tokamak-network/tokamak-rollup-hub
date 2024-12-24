a4f7d478f51cd9035fb099b8bfb9b135f1ece929
ssaid <swywssaid@gmail.com>
2024-12-24 14:28:36 +0900

    refactor: Separate SVG files and apply conditional styling to accordion SVGs

--- apps/web/components/accordion/accordion.tsx
+++ apps/web/components/accordion/accordion.tsx
@@ -19,55 +19,34 @@ export function Accordion({ title, content }: AccordionProps): JSX.Element {
         onClick={() => setIsOpen(!isOpen)}
       >
         <h3 className="text-[16px] font-semibold">{title}</h3>
-        {isOpen ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="12"
-            height="11"
-            viewBox="0 0 12 11"
-            fill="none"
-          >
-            <path
-              d="M10.5502 0.950195L1.4502 10.0502"
-              className={`${isOpen ? 'light:stroke-black dark:stroke-white' : ''}`}
-              strokeWidth="1.3"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              d="M1.4502 0.950195L10.5502 10.0502"
-              className={`${isOpen ? 'light:stroke-black dark:stroke-white' : ''}`}
-              strokeWidth="1.3"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="14"
-            height="15"
-            viewBox="0 0 14 15"
-            fill="none"
-          >
-            <path
-              d="M7 1.5V13.5"
-              stroke="#0070ED"
-              strokeWidth="1.32353"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              d="M13 7.5H1"
-              stroke="#0070ED"
-              strokeWidth="1.32353"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        )}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="14"
+          height="15"
+          viewBox="0 0 14 15"
+          fill="none"
+          className={`transition-transform duration-300 ${isOpen ? 'rotate-45' : 'rotate-0'}`}
+        >
+          <path
+            d="M7 1.5V13.5"
+            className={`transition-colors ${
+              isOpen ? 'light:stroke-black dark:stroke-white' : 'stroke-[#0070ED]'
+            }`}
+            strokeWidth="1.32353"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M13 7.5H1"
+            className={`transition-colors ${
+              isOpen ? 'light:stroke-black dark:stroke-white' : 'stroke-[#0070ED]'
+            }`}
+            strokeWidth="1.32353"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
       </div>
-
       <div
         className={`overflow-hidden transition-all duration-200 ease-in-out ${
           isOpen ? 'max-h-[500px] opacity-100' : 'max-h-0 opacity-0'
